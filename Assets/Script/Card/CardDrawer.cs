15a6454755a9857929808d708a1547ad6e70aecc
junwoo1903 <danny1903@naver.com>
2026-03-31 21:25:08 +0900

    3/31 전준우 작업물
    -현재 가진 카드 표시
    -카드 드로우 애니메이션 추가
    -카드 뒷면 이미지 적용
    -카드 클릭으로 선택 및 해제

--- Assets/Script/Card/CardDrawer.cs
+++ Assets/Script/Card/CardDrawer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class CardDrawer : MonoBehaviour
 {
@@ -11,35 +12,11 @@ public class CardDrawer : MonoBehaviour
 
     private void Start()
     {
-        DrawStartingHand(); 
+        StartCoroutine(CardDealAnimator.Instance.DealCardsAnimation(5));
     }
 
-
-    // 시작할 때 카드 5장 드로우
-    public void DrawStartingHand()
-    {
-        DrawMultipleCards(5);
-    }
-
-    // 카드 여러 장 드로우
-    public void DrawMultipleCards(int count)
-    {
-        for (int i = 0; i < count; i++)
-        {
-            DrawOneCard();
-        }
-    }
-
-    // 카드 한 장 드로우
     public void DrawOneCard()
     {
-        if (HandManager.Instance.GetHandCount() >= 5)
-        {
-            Debug.Log("패가 가득 찼습니다!");
-            return;
-        }
-
-        CardData card = DeckManager.Instance.DrawCard();
-        HandManager.Instance.AddCardToHand(card);
+        StartCoroutine(CardDealAnimator.Instance.DealCardsAnimation(1));
     }
 }
